013a0dad6b0f52a10bdaaf05bdb1dee0551df2d6
Braden Hancock <bradenfrompa>
2021-06-15 12:03:22 -0400

    rebuilt structure using express-generator

--- server/app.js
+++ server/app.js
@@ -1,34 +1,41 @@
-const express = require('express');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const PORT = process.env.PORT || 3001;
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const app = express();
+var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-var visits = 0;
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    visits += 1;
-    res.json({
-        pageName: 'Home Page',
-        visits: visits
-    });
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.get('/api', (req, res) => {
-    res.json({
-        pageName: 'Api Page'
-    });
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.get('/users', (req, res) => {
-    res.json({
-        pageName: 'Users Page'
-    });
-});
-    
-
-
-app.listen(PORT, () => {
-    console.log(`Server started on localhost:${PORT}`);
-});
+module.exports = app;
